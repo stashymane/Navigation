f54be74be6f657853cb51462e149bdbbc063794d
Albertas Šarutis <aestheticrice@gmail.com>
2018-07-23 12:05:33 +0300

    Restart project

--- README.md
+++
@@ -1,5 +0,0 @@
-bNavi
-=====
-
-The navigation plugin for Bukkit.
-Get help here -> http://gtsho.tk/nav
---
+++ src/main/java/co/stashcat/bNavi.java
@@ -0,0 +1,12 @@
+package co.stashcat;
+public class bNavi extends JavaPlugin {
+    public void onEnable()
+    {
+        saveDefaultConfig();
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
