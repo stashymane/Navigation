0f44238ddbdbc36b56163ae169d00922df3a26fa
Albertas Šarutis <aestheticrice@gmail.com>
2018-07-23 12:06:14 +0300

    Add static colored message sender

--- src/main/java/co/stashcat/bNavi.java
+++ src/main/java/co/stashcat/bNavi.java
@@ -1,4 +1,7 @@
 package co.stashcat;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 public class bNavi extends JavaPlugin {
     public void onEnable()
     {
@@ -8,5 +11,10 @@ public class bNavi extends JavaPlugin {
     public void onDisable()
     {
 
+    }
+
+    public static void sendMsg(CommandSender s, String msg)
+    {
+        s.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
     }
 }
