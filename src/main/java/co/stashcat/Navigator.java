7ccfb9314511f14f1304046412ea714e2f6a5cc7
Albertas Šarutis <aestheticrice@gmail.com>
2018-07-23 16:34:33 +0300

    Require waypoint for destination

--- src/main/java/co/stashcat/Navigator.java
+++ src/main/java/co/stashcat/Navigator.java
@@ -9,36 +9,27 @@ import java.util.Map;
 
 public class Navigator {
     static Map<Player, Location> compassState = new HashMap<>();
+    static Map<Player, Waypoint> destinations = new HashMap<>();
 
     public static void setDestination(Player p, Waypoint w) {
         setDestination(p, w.getLocation());
         Navigation.sendMsg(p, "&aNavigating to %s...", w.getName());
     }
 
-    public static void setDestination(Player p, Location loc) {
+    public static void setDestination(Player p, Waypoint loc) {
         saveCompassState(p);
+        destinations.put(p, loc);
         p.setCompassTarget(loc);
         Navigation.sendMsg(p, "&aNavigation target set to %d, %d.", loc.getBlockX(), loc.getBlockZ());
     }
 
     public static boolean hasReachedDestination(Location player, Waypoint dest) {
-        return hasReachedDestination(player, dest.getLocation(), dest.getDestinationRadius(), dest.isHeightIgnored());
-    }
-
-    public static boolean hasReachedDestination(Location player, Location dest) {
-        return hasReachedDestination(player, dest, 10);
-    }
-
-    public static boolean hasReachedDestination(Location player, Location dest, int destinationRadius) {
-        return hasReachedDestination(player, dest, destinationRadius, false);
-    }
-
-    public static boolean hasReachedDestination(Location player, Location dest, int destinationRadius, boolean ignoreHeight) {
-        if (ignoreHeight) {
+        Location temp = dest.getLocation();
+        if (dest.isHeightIgnored()) {
             player.setY(0);
-            dest.setY(0);
+            temp.setY(0);
         }
-        return player.distance(dest) < destinationRadius;
+        return player.distance(temp) < dest.getDestinationRadius();
     }
 
     public static boolean isNavigating(Player p) {
