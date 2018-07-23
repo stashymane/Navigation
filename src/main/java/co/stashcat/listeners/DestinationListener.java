4010240b78131aac1f142ed6e7081e1ab9d03e94
Albertas Šarutis <aestheticrice@gmail.com>
2018-07-23 15:16:48 +0300

    Add checkInterval setting

--- src/main/java/co/stashcat/listeners/DestinationListener.java
+++ src/main/java/co/stashcat/listeners/DestinationListener.java
@@ -1,8 +1,8 @@
 package co.stashcat.listeners;
 
+import co.stashcat.Navigation;
 import co.stashcat.Navigator;
 import co.stashcat.Tracker;
-import co.stashcat.Navigation;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -14,13 +14,15 @@ import java.util.Map;
 
 public class DestinationListener implements Listener {
     static Map<Player, Long> lastCheck = new HashMap<>();
+    Navigation pl;
 
     public DestinationListener(Navigation p) {
         Bukkit.getPluginManager().registerEvents(this, p);
+        pl = p;
     }
 
-    public static void destinationListener(PlayerMoveEvent e) {
-        if (Navigator.isNavigating(e.getPlayer()) && !Tracker.isTracking(e.getPlayer()) && (!lastCheck.containsKey(e.getPlayer()) || System.currentTimeMillis() - lastCheck.get(e.getPlayer()) > 1000)) {
+    public void destinationListener(PlayerMoveEvent e) {
+        if (Navigator.isNavigating(e.getPlayer()) && !Tracker.isTracking(e.getPlayer()) && (!lastCheck.containsKey(e.getPlayer()) || System.currentTimeMillis() - lastCheck.get(e.getPlayer()) > pl.getConfig().getDouble("checkInterval") * 1000)) {
             Player p = e.getPlayer();
             Location loc = p.getLocation();
             lastCheck.put(p, System.currentTimeMillis());
