d8e11f1ca7ea6735f0c084a0af55fe152797c1dc
Albertas Šarutis <aestheticrice@gmail.com>
2018-07-23 12:09:46 +0300

    Add Navigator class

---
+++ src/main/java/co/stashcat/Navigator.java
@@ -0,0 +1,37 @@
+package co.stashcat;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Navigator {
+    static Map<Player, Location> compassState = new HashMap<>();
+
+    public static void setDestination(Player p, Location loc) {
+        saveCompassState(p);
+        p.setCompassTarget(loc);
+        bNavi.sendMsg(p, "&aNavigation target set to " + loc.getBlockX() + ", " + loc.getBlockZ() + ".");
+    }
+
+    public static boolean hasReachedDestination(Location player, Location destination) {
+        return player.distance(destination) < 10;
+    }
+
+    public static boolean isNavigating(Player p) {
+        return compassState.containsKey(p);
+    }
+
+    public static void saveCompassState(Player p) {
+        if (!compassState.containsKey(p))
+            compassState.put(p, p.getCompassTarget());
+    }
+
+    public static void returnCompassState(Player p) {
+        if (compassState.containsKey(p)) {
+            p.setCompassTarget(compassState.get(p));
+            compassState.remove(p);
+        }
+    }
+}
