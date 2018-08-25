4b2076abbd0c1af3c6b5ffdaa68b6a17d796ab66
Albertas Šarutis <aestheticrice@gmail.com>
2018-08-25 12:47:44 +0300

    Remove debug line
    oops

--- src/main/java/co/stashcat/navigation/commands/WaypointTabCompleter.java
+++ src/main/java/co/stashcat/navigation/commands/WaypointTabCompleter.java
@@ -2,7 +2,6 @@ package co.stashcat.navigation.commands;
 
 import co.stashcat.navigation.WaypointManager;
 import co.stashcat.navigation.types.Waypoint;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -16,7 +15,6 @@ import java.util.List;
 public class WaypointTabCompleter implements TabCompleter {
     public List<String> onTabComplete(CommandSender s, Command cmd, String label, String[] args) {
         List<String> c = new ArrayList<>();
-        Bukkit.getLogger().info(args[args.length - 1]);
         if (args.length == 1) {
             c.addAll(Arrays.asList("new", "edit", "review", "set", "save", "cancel", "delete"));
         } else if (args.length == 2 && (args[0].equalsIgnoreCase("edit") || args[0].equalsIgnoreCase("review") || args[0].equalsIgnoreCase("delete"))) {
